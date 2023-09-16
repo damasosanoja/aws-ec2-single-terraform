3469b6f82d57884cb9ce621e058a81431a495b9d
damasosanoja <damasosanoja@gmail.com>
2023-09-16 10:31:41 -0400

    Bootstrap commit

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "ami_name" {
+  type = string
+  default = "Ubuntu_22.04"
+}
+variable "ami_id" {
+  type = string
+  default = "ami-053b0d53c279acc90"
+}
+variable "ami_key_pair_name" {
+  type = string
+  default = "loft-tutorial"
+}
+variable "ami_type" {
+  type = string
+  default = "t2.micro"
+}
